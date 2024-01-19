d640c993e3a562f6ba1cd366fbc6de6be55c5ab6
Garnik Papyan <garnikp1997@gmail.com>
2024-01-19 04:27:52 +0400

    BookMaster_BD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,59 +8,106 @@ public class Main {
         String url = "jdbc:postgresql://localhost:5432/BookStore_DB";
         String user = "postgres";
         String password = "admin077";
-        // connecting sql server
+
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             System.out.println("Successful connection to PostgreSQL ");
             Books books = new Books(connection);
             CustomerManagement customerManagement = new CustomerManagement(connection);
-          //  books.createListGenre();
-//            books.addNewRow();
-//            books.deleteRow();
-//            books.createListGenre();
-//            books.updateBookDetails();
-            //customerManagement.deleteRow();
-            customerManagement.viewPurchaseHistory();
-           // commandLine();
-            //Retrieve a list of all books sold, including the book title, customer
-            //name, and date of sale..
-            String sqlQuery =   "SELECT books.Title,  Customers.Name, Sales.DateOfSale " +
-                    "FROM Sales " + "JOIN books ON Sales.bookID = books.bookID " +
-                    "JOIN customers ON Sales.CustomerID = customers.CustomerID ";
-            //Find the total revenue generated from each genre of books.
-            String sqlAggregation = "SELECT books.Genre, SUM(Sales.TotalPrice) AS SumPrice " +
-                    "FROM Sales " + "JOIN books ON Sales.bookID = books.bookID " +
-                    "GROUP BY books.Genre ";
-           String sql = "SELECT * FROM books ";
-            // here you can already make different requests for sqlQuery
-           // int count = 7;
-           // int id = 4;
-          // updateBookDetails(connection,count,id);
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+            SalesProcessing salesProcessing = new SalesProcessing(connection);
 
-                 ResultSet resultSet = preparedStatement.executeQuery()) {
+            boolean out = false;
+            while (!out) {
+                System.out.println("You can choose from these three options what to do ");
+                System.out.print("1. Book Management \n");
+                System.out.print("2. Customer  Management \n");
+                System.out.print("3. Sales Management \n");
+                Scanner scanner = new Scanner(System.in);
+                String str =  scanner.nextLine();
+                switch (str) {
+                    case "1" -> {
+                        boolean out2 = false;
+                        while (!out2) {
+                            System.out.print("\n");
+                            System.out.println("In Book Management you can update books " +
+                                    "details ,see list of book from genre,add and delete books ");
+                            System.out.print("1. Update books details \n");
+                            System.out.print("2. Create list from genre \n");
+                            System.out.print("3. Add new book \n");
+                            System.out.print("4. Delete book \n");
+                            System.out.print("5. Out(Back one step) \n");
+                            System.out.print("6. Exit Program \n");
+                            String str2 = scanner.nextLine();
+                            switch (str2) {
+                                case "1" -> books.updateBookDetails();
+                                case "2" -> books.createListGenre();
+                                case "3" -> books.addNewRow();
+                                case "4" -> books.deleteRow();
+                                case "5" -> out2 = true;
+                                case "6" -> {
+                                    out = true;
+                                    out2 = true;
+                                }
+                                default -> System.out.println("Input 1,2,3,4,5 or 6 ");
+                            }
+                        }
+                    }
+                    case "2" -> {
+                        boolean out3 = false;
+                        while (!out3) {
+                            System.out.print("\n");
+                            System.out.println("In Customer Management you can update customer " +
+                                    "details ,view purchase history,add and delete customer ");
+                            System.out.print("1. Update customer details \n");
+                            System.out.print("2. View purchase history \n");
+                            System.out.print("3. Add new customer \n");
+                            System.out.print("4. Delete customer \n");
+                            System.out.print("5. Out(Back one step) \n");
+                            System.out.print("6. Exit Program \n");
+                            String custm = scanner.nextLine();
+                            switch (custm) {
+                                case "1" -> customerManagement.customerDetailsUpdate();
+                                case "2" -> customerManagement.viewPurchaseHistory();
+                                case "3" -> customerManagement.addNewRow();
+                                case "4" -> customerManagement.deleteRow();
+                                case "5" -> out3 = true;
+                                case "6" -> {
+                                    out3 = true;
+                                    out = true;
+                                }
+                                default -> System.out.println("Input 1,2,3,4,5 or 6 ");
+                            }
+                        }
+                    }
+                    case "3" -> {
+                        boolean out4 = false;
+                        while (!out4) {
+                            System.out.print("\n");
+                            System.out.println("In Sales Management you can buying new books, " +
+                                    "generate a report of all books sold,calculate total revenue by genre ");
+                            System.out.print("1. Buying new books");
+                            System.out.print("2. Report of all sold books");
+                            System.out.print("3. Total revenue by genre");
+                            System.out.print("4. Out(Back one step)");
+                            System.out.print("5. Exit program");
+                            String sell = scanner.nextLine();
+                            switch (sell) {
+                                case "1" -> salesProcessing.newSales();
+                                case "2" -> salesProcessing.reportOfAllBookSold();
+                                case "3" -> salesProcessing.revenueByGenre();
+                                case "4" -> out4 = true;
+                                case "5" -> {
+                                    out4 = true;
+                                    out = true;
+                                }
+                                default -> System.out.println("Input 1,2,3,4,5 or 6 ");
+                            }
+                        }
 
-               // createTrigger(connection);
-                while (resultSet.next()) {
-                    String bookTitle = resultSet.getString("Title");
-                    String customerName = resultSet.getString("Name");
-                    String dateOfSale = resultSet.getString("DateOfSale");
-
-                //    System.out.println("Book: " + bookTitle +" " + "Customer Name: " + customerName + " " + "Date of Sale: " + dateOfSale);
-                    System.out.println();
+                    }
+                    default -> System.out.println("Plz enter 1 , 2 or 3");
                 }
             }
-
-            // here you can already make different requests for sqlAggregation
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlAggregation);
-                 ResultSet resultSet = preparedStatement.executeQuery()) {
-                while (resultSet.next()) {
-
-                    String bookGenre = resultSet.getString("Genre");
-                    double totalPrice = resultSet.getDouble("SumPrice");
-                  //  System.out.println("Book Genre: " + bookGenre + " = " + totalPrice + "$");
-
-                }
-            }
+            System.out.println("the end");
 
         } catch (SQLException e) {
             System.out.println("Connection error to PostgreSQL " + e.getMessage() );
@@ -85,76 +132,5 @@ public class Main {
             statement.execute(sqlFunction);
             statement.execute(sqlTrigger);
         }
-
     }
-
-//    public static void updateBookDetails(Connection connection,int quantityInStock,int bookId) throws SQLException {
-//        String sqlUpdate = "UPDATE Books SET QuantityInStock = ? WHERE BookId = ? ";
-//        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlUpdate)) {
-//            preparedStatement.setInt(1,quantityInStock);
-//            preparedStatement.setInt(2,bookId);
-//            preparedStatement.executeUpdate();
-//        }
-//    }
-
-//    public static void createListGenre(Connection connection,String genre) throws SQLException {
-//        String sqlGenre = "SELECT * FROM books WHERE Genre = ? ";
-//        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlGenre)) {
-//            preparedStatement.setString(1,genre);
-//            try(ResultSet resultSet = preparedStatement.executeQuery()) {
-//                System.out.print("We have books from this Genre- " + genre + "\n");
-//                while (resultSet.next()) {
-//                            System.out.println(" Title- " + resultSet.getString("Title")
-//                            + ", Author- " + resultSet.getString("Author"));
-//                }
-//            }
-//        }
-//    }
-
-    public static void commandLine() {
-        System.out.println("You can choose from these three options what to do ");
-        System.out.print("1. Book Management \n");
-        System.out.print("2. Customer  Management \n");
-        System.out.print("3. Sales Management \n");
-        Scanner scanner = new Scanner(System.in);
-        boolean out = false;
-        while (!out) {
-            String str =  scanner.nextLine();
-            switch (str) {
-                case "1" -> {
-                    System.out.println("In Book Management you can update books " +
-                            "details or see list of book from genre ");
-                    System.out.print("1. Update books details \n");
-                    System.out.print("2. Create list from genre \n");
-                    while (!out) {
-                        String str2 = scanner.nextLine();
-                        switch (str2) {
-                            case "1" -> {
-                                System.out.println("Enter BookId ");
-                                out = true;
-                            }
-                            case "2" -> {
-                                System.out.println("boo");
-                                out = true;
-                            }
-                            default -> System.out.println("1 kam 2");
-                        }
-                    }
-
-                    out = true;
-                }
-                case "2" -> {
-                    System.out.println("ddd");
-                    out = true;
-                }
-                case "3" -> {
-                    System.out.println("www");
-                    out = true;
-                }
-                default -> System.out.println("Plz enter 1 , 2 or 3");
-            }
-        }
-        System.out.println("the end");
-    }
-
 }
--- src/main/java/org/example/SalesProcessing.java
+++ src/main/java/org/example/SalesProcessing.java
@@ -1,4 +1,164 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class SalesProcessing {
+    private final Connection connection;
+    public SalesProcessing(Connection connection) {
+        this.connection = connection;
+    }
+    Scanner scanner = new Scanner(System.in);
+
+    public void newSales() throws SQLException {
+        String sqlNewSale = "INSERT INTO sales(BookID,CustomerID,DateOfSale,QuantitySold,TotalPrice) " +
+                "VALUES (?,?,?,?,?) " ;
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlNewSale)){
+            boolean out = false;
+            System.out.println("If you want buy enter ` BookId");
+            int bookId = -1;
+            int customerId = -1;
+            String dateOfSale = "";
+            int quantitySold = -1;
+                while (!out) {
+                    try{
+                        bookId = scanner.nextInt();
+                        if(validBookId(bookId)) {
+                        while (!out) {
+                            try {
+                                System.out.println("enter customer ID ` ");
+                                customerId = scanner.nextInt();
+                                if(validCustomerId(customerId)) {
+                                    scanner.nextLine();
+                                    System.out.println("Enter buying day in this format ` YYYY-MM-DD ");
+                                    dateOfSale = scanner.nextLine();
+                                    while (!out) {
+                                        System.out.println("Enter how many pieces of this book you want");
+                                        try{
+                                            quantitySold = scanner.nextInt();
+                                            if(quantitySold > 0 && quantitySold <=getQuantityInStock(bookId)) {
+                                                out = true;
+                                            } else {
+                                                System.out.println("there are so many books out of stock, try again");
+                                            }
+                                        } catch (InputMismatchException e) {
+                                            System.out.println("Plz enter only NUMBER how much you want buy");
+                                            scanner.next();
+                                        }
+                                    }
+                                }else {
+                                    System.out.println("Plz enter valid customer ID");
+                                }
+                            } catch (InputMismatchException e) {
+                                System.out.println("Plz enter Number for Customer ID");
+                                scanner.next();
+                            }
+                        }
+                    } else {
+                        System.out.println("Plz enter valid book ID");
+                    }
+                } catch (InputMismatchException e) {
+                        System.out.println("enter only Number from ID");
+                        scanner.next();
+                    }
+            }
+            preparedStatement.setInt(1,bookId);
+            preparedStatement.setInt(2,customerId);
+            preparedStatement.setString(3,dateOfSale);
+            preparedStatement.setInt(4,quantitySold);
+            double totalPrice = getBookPrice(bookId)*quantitySold;
+            preparedStatement.setDouble(5,totalPrice);
+            preparedStatement.executeUpdate();
+            System.out.println("Buying was successful");
+        }
+    }
+
+    public void revenueByGenre() throws SQLException {
+        String sqlAggregation = "SELECT books.Genre, SUM(Sales.TotalPrice) AS SumPrice " +
+                "FROM Sales " + "JOIN books ON Sales.bookID = books.bookID " +
+                "GROUP BY books.Genre ";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlAggregation);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+             System.out.println("Here are the books sold by genre");
+            while (resultSet.next()) {
+                String bookGenre = resultSet.getString("Genre");
+                double totalPrice = resultSet.getDouble("SumPrice");
+                System.out.println("Book Genre: " + bookGenre + " = " + totalPrice + "$");
+            }
+        }
+    }
+
+    public void reportOfAllBookSold() throws SQLException {
+        String sqlReport =   "SELECT books.Title,  Customers.Name, Sales.DateOfSale " +
+                "FROM Sales " + "JOIN books ON Sales.bookID = books.bookID " +
+                "JOIN customers ON Sales.CustomerID = customers.CustomerID ";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlReport);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                String bookTitle = resultSet.getString("Title");
+                String customerName = resultSet.getString("Name");
+                String dateOfSale = resultSet.getString("DateOfSale");
+                System.out.println("Book: " + bookTitle +" " + "Customer Name: " + customerName + " " + "Date of Sale: " + dateOfSale);
+                System.out.println();
+            }
+        }
+    }
+
+    public boolean validBookId(int bookid) throws SQLException {
+        String sqlValidBook = "SELECT BookID FROM books ";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlValidBook);
+            ResultSet resultSet = preparedStatement.executeQuery()) {
+            List <Integer> myList = new ArrayList<>();
+            while (resultSet.next()) {
+                myList.add(resultSet.getInt("BookID"));
+            }
+            return myList.contains(bookid);
+        }
+    }
+
+    public boolean validCustomerId(int customerid) throws SQLException{
+        String sqlValidCustomer = "SELECT CustomerID FROM customers ";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlValidCustomer);
+            ResultSet resultSet = preparedStatement.executeQuery()) {
+            List <Integer> myList = new ArrayList<>();
+            while (resultSet.next()) {
+                myList.add(resultSet.getInt("CustomerID"));
+            }
+            return myList.contains(customerid);
+        }
+    }
+
+    public int getQuantityInStock(int bookId) throws SQLException {
+        String sqlQuantity = "SELECT QuantityInStock FROM books WHERE BookID = ?";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlQuantity)) {
+           preparedStatement.setInt(1,bookId);
+           try(ResultSet resultSet = preparedStatement.executeQuery()){
+               if(resultSet.next()) {
+                   return resultSet.getInt("QuantityInStock");
+               }
+           }
+        }
+        return  -1;
+    }
+
+    public double getBookPrice(int BookId) throws SQLException {
+        String sqlPrice = "SELECT Price FROM books WHERE BookID = ? ";
+        double price = 0.0;
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sqlPrice)) {
+            preparedStatement.setInt(1,BookId);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+                if(resultSet.next()) {
+                    price = resultSet.getDouble("Price");
+                }
+            }
+            return price;
+        }
+    }
+
 }
